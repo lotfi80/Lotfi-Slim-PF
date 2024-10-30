786ca6786508371349bbf3f38ef45c9b7c8f3c28
lotfi80 <salioslim@hotmail.de>
2024-10-31 00:49:58 +0100

    Erste Teil Fertig : muss das bild Lotfi anpassen und die icons bei dark mode korriegieren

---
+++ meinPF/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Lotfi from './sections/lotfi/Lotfi'
+
+function App() {
+
+  return (
+    <div>
+      <Lotfi />
+    </div>
+  )
+}
+
+export default App
---
+++ meinPF/src/sections/lotfi/Lotfi.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import styles from "./LotfiStyle.module.css";
+import lotfiImg from "../../assets/lotfi.png";
+import darkMode from "../../assets/darkMode.svg";
+import lightMode from "../../assets/lightMode.svg";
+import twitterDark from "../../assets/twitterDark.svg";
+import twitterLight from "../../assets/twitterLight.svg";
+import linkedinDark from "../../assets/linkedinDark.svg";
+import gitHubDark from "../../assets/githubDark.svg";
+import gitHubLight from "../../assets/githubLight.svg";
+import linkedinLight from "../../assets/linkedinLight.svg";
+import CV from "../../assets/CV.pdf";
+import { useTheme } from "../../common/ThemeContext";
+
+function Lotfi() {
+  const { theme, toggleTheme } = useTheme();
+  const themeIcon = theme === "light" ? lightMode : darkMode;
+  const twitterIcon = theme === "light" ? twitterLight : twitterDark;
+  const linkedinIcon = theme === "light" ? linkedinLight : linkedinDark;
+  const gitHubIcon = theme === "light" ? gitHubLight :gitHubDark ;
+
+  return (
+    <section id="lotfi" className={styles.colorModeContainer}>
+      <div className={styles.colorModeContainer}>
+        <img
+          className={styles.colorMode}
+          src={lotfiImg}
+          alt="Profile picture of Lotfi Slim"
+        />
+        <img src={themeIcon} alt="color mode icon" onClick={toggleTheme} />
+      </div>
+      <div className={styles.info}>
+        <h1>
+          Lotfi
+          <br />
+          Slim
+        </h1>
+        <h2>Frontend Developper</h2>
+        <span>
+          <a href="https://www.linkedin.com/in/lotfi-slim-de/" target="_blank">
+            <img src={linkedinIcon} alt="" />
+          </a>
+          <a href="https://github.com/lotfi80" target="_blank">
+            <img src={gitHubIcon} alt="" />
+          </a>
+          <a href="https://www.linkedin.com/in/lotfi-slim-de/" target="_blank">
+            <img src={twitterIcon} alt="" />
+          </a>
+        </span>
+        <p className="description">
+          with a passion for developping modern React web apps for commercial
+          business
+        </p>
+        <a href={CV} download>
+          <button className="hover">Resume</button>
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default Lotfi;
